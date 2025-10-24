ee4e642dc7224d3a33e4af0edf08aef99799486e
zaqimaulana <zaqimaulana72@gmail.com>
2025-10-24 23:11:17 +0700

    menambahkan images

--- lib/main.dart
+++ lib/main.dart
@@ -37,11 +37,11 @@ class MyHome extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // 🔄 Container 1 dengan efek rotasi 15 derajat
+          // 🔄 Container 1
           Transform.rotate(
-            angle: 0.26, // 0.26 radian = 15 derajat
+            angle: 0.26, // 15 derajat
             child: Container(
-              width: 120,
+              width: 220,
               height: 120,
               alignment: Alignment.center,
               margin: const EdgeInsets.only(top: 30, left: 20),
@@ -65,16 +65,19 @@ class MyHome extends StatelessWidget {
               child: const Text(
                 "Ini adalah teks di dalam container",
                 textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
               ),
             ),
           ),
 
-        
+          // 🔄 Container 2
           Transform.rotate(
-            angle: -0.17, // -0.17 radian = -10 derajat
+            angle: -0.17, // -10 derajat
             child: Container(
-              width: 120,
+              width: 180,
               height: 120,
               alignment: Alignment.center,
               margin: const EdgeInsets.only(top: 30, left: 20),
@@ -99,6 +102,39 @@ class MyHome extends StatelessWidget {
                 "Ini King Emyu",
                 textAlign: TextAlign.center,
                 style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+
+          // 🖼️ Gambar di bawah "Ini King Emyu"
+          Transform.rotate(
+            angle: 0.1, // sedikit miring agar serasi
+            child: Container(
+              width: 160,
+              height: 160,
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(top: 30, left: 20),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.purple, Colors.orange],
+                ),
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 3,
+                    blurRadius: 6,
+                    offset: const Offset(3, 3),
+                  ),
+                ],
+              ),
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
               ),
             ),
           ),
