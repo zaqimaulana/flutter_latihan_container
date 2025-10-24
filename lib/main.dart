36032efb7617cafeb7f08d0542ef3e2cdaab98d7
zaqimaulana <zaqimaulana72@gmail.com>
2025-10-24 21:31:56 +0700

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-// Methode MyApp
+// Method MyApp
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Membuat aplikasi baru",
-      home: MyHome(),
+      home: const MyHome(),
     );
   }
 }
 
-// method MyHome
+// Method MyHome
 class MyHome extends StatelessWidget {
   const MyHome({super.key});
 
@@ -26,9 +26,9 @@ class MyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Latihan Container", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.blueGrey,
-        actions: [
+        title: const Text("Latihan Container", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+        actions: const [
           Icon(Icons.person, color: Colors.white),
           SizedBox(width: 10),
           Icon(Icons.account_tree, color: Colors.white),
@@ -36,6 +36,18 @@ class MyHome extends StatelessWidget {
           Icon(Icons.ac_unit, color: Colors.white),
           SizedBox(width: 20),
         ],
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(top: 30),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: const Text(
+          "Hello Container!",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
